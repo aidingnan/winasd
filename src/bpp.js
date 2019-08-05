fae089e8c8f9ba3f9fb0d3858d94b1ae512babba
matianfu <matianfu@gmail.com>
2019-08-05 19:29:05 +0800

    update bpp.js, correct typo, slice base64 string

--- src/bpp.js
+++ src/bpp.js
@@ -2,28 +2,47 @@ const Config = require('config')
 const initEcc = require('./lib/atecc')
 
 let index = process.argv.findIndex(x => x === '--code')
-if (index == -1) throw new Error('--code required')
-
-if (process.argv < index + 2) throw new Error('code must be provided')
+if (index === -1 || index === process.argv.length - 1) {
+  console.log('usage: node src/bpp.js --code xxxx')
+  process.exit(1)
+}
 
 const code = process.argv[index + 1]
-
-if (!code || !code.length) throw new Error('invaild code')
-
+if (!code || !code.length || code.length > 128) {
+  console.log('invalid code')
+  process.exit(1)
+}
 
 initEcc(Config.ecc.bus, (err, ecc) => {
   if (err) throw err
-  ecc.preset(e => {
-    if (e) throw e
+  ecc.preset(err => {
+    if (err) throw err
     ecc.serialNumber({}, (err, sn) => {
       if (err) throw err
-      ecc.genCsr({ o: 'Shanghai Dingnan Co., Ltd.', cn: 'PocketDrive', serialNumber: sn, ou: code }, (err, der) => {
-        if (err) throw err
-        let pem = '-----BEGIN CERTIFICATE REQUEST-----\n'
-              + der.toString('base64') + '\n'
-              + '-----END CERTIFICATE REQUEST-----\n'
-        console.log(pem)
-      })
+      if (!/^0123[0-9a-f]{12}ee$/.test(sn)) {
+        console.log('bad sn')
+        process.exit(1)
+      } else {
+        ecc.genCsr({
+          o: 'Shanghai Dingnan Co., Ltd.',
+          cn: 'IntelliDrive',
+          ou: code,
+          serialNumber: sn
+        }, (err, der) => {
+          if (err) throw err
+          let b64 = der.toString('base64')
+          let ls = []
+          while (b64.length > 64) {
+            ls.push(b64.slice(0, 64))
+            b64 = b64.slice(64)
+          }
+          ls.push(b64)
+          ls.unshift('-----BEGIN CERTIFICATE REQUEST-----')
+          ls.push('-----END CERTIFICATE REQUEST-----')
+          console.log(ls.map(l => l + '\n').join(''))
+        })
+      }
     })
   })
 })
+
