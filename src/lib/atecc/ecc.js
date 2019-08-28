54955075f35d6bda5d790d8216625fe918f5ed53
matianfu <matianfu@gmail.com>
2019-08-28 18:49:33 +0800

    hijack readCounter and incCounter with new method

--- src/lib/atecc/ecc.js
+++ src/lib/atecc/ecc.js
@@ -318,6 +318,14 @@ class ECC {
       .then(c => callback(null, c))
       .catch(e => callback(e))
   }
+
+  readCounter(opts, callback) {
+    this.counter('read', 0, callback)
+  }
+
+  incCounter(opts, callback) {
+    this.counter('incr', 0, callback)
+  }
 }
 
 Object.assign(ECC.prototype, commands)
