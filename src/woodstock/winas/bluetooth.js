70aacc8e1f58e7d43aedf87f3bf956daa71cb09b
CJackYang <ijackyang@sina.cn>
2019-08-28 19:40:55 +0800

    tmp

--- src/woodstock/winas/bluetooth.js
+++ src/woodstock/winas/bluetooth.js
@@ -1,5 +1,4 @@
 const DBusObject = require('../lib/dbus-object')
-const DBusProperties = require('../lib/dbus-properties')
 const DBusObjectManager = require('../lib/dbus-object-manager')
 const Advertisement = require('../bluez/advertisement')
 const GattSerialService = require('../bluez/serives/gatt-serial-service')
@@ -14,12 +13,12 @@ const GattAccessPointService = require('../bluez/serives/gatt-access-point-servi
  * BLE_DEVICE_CONNECTED
  */
 class Bluetooth extends DBusObject {
-  constructor(bound, localName = 'noname') {
+  constructor (boundF, sn, hostname) {
     super()
-    let b = bound ? 0x02 : 0x01
-    // let s = sn ? sn.slice(-8) : ''
-    // this.localName = hostname || s
-    this.localName = localName
+    this.boundF = boundF
+    const b = boundF() ? 0x02 : 0x01
+    const s = sn ? sn.slice(-8) : ''
+    this.localName = hostname || s
     this.adv = new Advertisement('advertisement0', {
       Type: 'peripheral',
       LocalName: this.localName,
@@ -34,53 +33,53 @@ class Bluetooth extends DBusObject {
 
     this.addChild(this.adv)
 
-    //100 NIC
-    let NICService = new GattNICService('service4', true)
+    // 100 NIC
+    const NICService = new GattNICService('service4', true)
     NICService.on('Char1WriteValue', (...args) => this.emit('NICChar1Write', ...args))
     NICService.on('Char2WriteValue', (...args) => this.emit('NICChar2Write', ...args))
     this.NICChar1Update = NICService.char1Update.bind(NICService.rxIface)
     this.NICChar2Update = NICService.char2Update.bind(NICService.rxIface)
 
-    //200 AP
-    let APService = new GattAccessPointService('service5', true)
+    // 200 AP
+    const APService = new GattAccessPointService('service5', true)
     APService.on('Char1WriteValue', (...args) => this.emit('APChar1Write', ...args))
     APService.on('Char2WriteValue', (...args) => this.emit('APChar2Write', ...args))
     this.APChar1Update = APService.char1Update.bind(APService.rxIface)
     this.APChar2Update = APService.char2Update.bind(APService.rxIface)
 
-    //600 LocalAuth
-    let service1 = new GattLocalAuthService('service1', true)
-    service1.on('WriteValue', (...args) => this.emit('Service1Write', ...args))
-    this.Service1Update = service1.rxIface.update.bind(service1.rxIface)
+    // 600 LocalAuth
+    const service1 = new GattLocalAuthService('service1', true)
+    service1.on('WriteValue', (...args) => this.emit('LocalAuthWrite', ...args))
+    this.LocalAuthUpdate = service1.rxIface.update.bind(service1.rxIface)
 
     // 700 NetworkSetting
-    let service2 = new GattNetworkSettingService('service2', true)
-    service2.on('WriteValue', (...args) => this.emit('Service2Write', ...args))
-    this.Service2Update = service2.rxIface.update.bind(service2.rxIface)
+    const service2 = new GattNetworkSettingService('service2', true)
+    service2.on('WriteValue', (...args) => this.emit('NSWrite', ...args))
+    this.NSUpdate = service2.rxIface.update.bind(service2.rxIface)
 
     // 800 Cloud
-    let service3 = new GattSerialService('service3', true)
-    service3.on('WriteValue', (...args) => this.emit('Service3Write', ...args))
-    this.Service3Update = service3.rxIface.update.bind(service3.rxIface)
+    const service3 = new GattSerialService('service3', true)
+    service3.on('WriteValue', (...args) => this.emit('CloudWrite', ...args))
+    this.CloudUpdate = service3.rxIface.update.bind(service3.rxIface)
 
     // gatt root
-    let gatt = new DBusObject('gatt')
+    const gatt = new DBusObject('gatt')
       .addInterface(new DBusObjectManager())
       .addChild(service1)
 
-    let gatt1 = new DBusObject('gatt1')
+    const gatt1 = new DBusObject('gatt1')
       .addInterface(new DBusObjectManager())
       .addChild(service2)
 
-    let gatt2 = new DBusObject('gatt2')
+    const gatt2 = new DBusObject('gatt2')
       .addInterface(new DBusObjectManager())
       .addChild(service3)
 
-    let NICGATT = new DBusObject('gatt3')
+    const NICGATT = new DBusObject('gatt3')
       .addInterface(new DBusObjectManager())
       .addChild(NICService)
 
-    let APGATT = new DBusObject('gatt4')
+    const APGATT = new DBusObject('gatt4')
       .addInterface(new DBusObjectManager())
       .addChild(APService)
 
@@ -92,11 +91,11 @@ class Bluetooth extends DBusObject {
       .addChild(APGATT)
   }
 
-  updateAdv(bound, localName) {
-    let b = bound ? 0x02 : 0x01
+  updateAdv () {
+    const b = this.boundF() ? 0x02 : 0x01
     this.adv.updateAdv({
       Type: 'peripheral',
-      LocalName: localName || this.localName,
+      LocalName: this.localName,
       ManufacturerData: [
         [0xffff, ['ay', [b]]]
       ],
@@ -104,7 +103,7 @@ class Bluetooth extends DBusObject {
     })
   }
 
-  mounted() {
+  mounted () {
     super.mounted()
     this.dbus.listen({
       sender: 'org.bluez',
@@ -112,7 +111,7 @@ class Bluetooth extends DBusObject {
     }, this.listen.bind(this))
   }
 
-  listen(m) {
+  listen (m) {
     // device add / remove
     if (m.path.startsWith('/org/bluez/hci0/') && m.interface === 'org.bluez.Device1') {
       if (m.changed && m.changed.hasOwnProperty('ServicesResolved')) {
