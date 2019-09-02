dd6100dd1456613d13c1316a955e3a39d8e0b677
matianfu <matianfu@gmail.com>
2019-09-02 21:26:49 +0800

    wip

---
+++ src/components/sata.js
@@ -0,0 +1,122 @@
+const path = require('path')
+const fs = require('fs')
+const EventEmitter = require('events')
+
+const uuid = require('uuid')
+const mkdirp = require('mkdirp')
+
+const btrfsStat = require('../lib/btrfsStat')
+
+/**
+Sata object provides:
+1. status 0x00, 0x02..., 0x80, 0xFF
+2. statusUpdate event
+
+this 
+*/
+class Sata extends EventEmitter {
+  constructor () {
+    super()
+    this.status = 0x00
+    this.mp = path.join('/run', uuid.v4())
+   
+    this.busy = true 
+    mkdirp(this.mp, err => {
+      if (err) {
+        this.busy = false
+        this.setStatus(0xff)
+      } else {
+        this.checkSdaAsync()
+          .then(status => { 
+            this.busy = false 
+            this.setStatus(status)
+          })
+          .catch(e => { 
+            this.busy = false 
+            this.setStatus(0xff)
+          })
+      }
+    })
+  }
+
+  // internal method
+  setStatus (status) {
+    this.status = status
+    process.nextTick(() => this.emit('statusUpdate', status))
+  }
+
+  // internal method
+  async checkSdaAsync () {
+    return new Promise((resolve, reject) => {
+      btrfsStat(this.mp, (err, status) => {
+        // This is weird TODO
+        if (status) {
+          resolve(status)
+        } else {
+          reject(err)
+        }
+      })
+    }) 
+  }
+
+  async formatAsync () {
+    if (![0x03, 0x04, 0x80].includes(this.status)) {
+      let err = new Error(`operation forbidden for status ${this.status}`) 
+      err.code = 'EFORBIDDEN'
+      throw err
+    }
+
+    if (this.status === 0x80) {
+      const stdout = await childExecAsync('cat /proc/mounts')
+      const mnts = stdout.toString()
+        .split('\n')
+        .map(l => l.trim())
+        .filter(l => l.length)
+        .map(l => {
+          // <file system> <mount point>   <type>  <options>       <dump>  <pass>
+          let flds = l.split(' ').map(fld => fld.trim())
+          return {
+            fs: flds[0],
+            mp: flds[1],
+            type: flds[2],
+            opts: flds[3],
+            dump: flds[4],
+            pass: flds[5]
+          }
+        })
+
+      if (mnts.find(m => m.fs = '/dev/sda' && m.type === 'btrfs')) {
+        await childExecAsync('umount -f /dev/sda')  
+      }
+    }
+
+    await childExecAsync('mkfs.btrfs -f /dev/sda')
+    await childExecAsync('partprobe')
+  }
+
+  format (callback) {
+    if (this.busy) {
+      let err = new Error('busy')
+      err.code = 'EBUSY'
+      process.nextTick(() => callback(err))
+    } else {
+      this.busy = true
+      this.formatAsync()
+        .then(() => callback(null))
+        .catch(e => callback(e))
+        .then(() => {
+          this.checkSdaAsync()
+            .then(status => {
+              this.busy = false
+              this.setStatus(status)
+            })
+            .catch(e => {
+              this.busy = false
+              this.setStatus(0xff)
+            })
+        })
+    }
+  } 
+}
+
+module.exports = new Sata()
---
+++ src/lib/atecc/atecc.js
@@ -0,0 +1,132 @@
+const EventEmitter = require('events')
+
+const i2c = require('i2c-bus')
+const config = require('config')
+
+const ECC = require('./ecc')
+const { Signature } = require('./csr')
+
+class Wrapper extends EventEmitter {
+  constructor (busNum) {
+    super()
+    this.busNum = busNum
+    this.ecc = null
+    this.error = null
+    this.q = []
+
+    // fake op as placeholder
+    this.q.push({})
+    this.init(busNum, (err, ecc) => {
+      // remove placeholder
+      this.q.shift()
+      if (err) {
+        err.code = 'EUNAVAIL'
+        this.error = err
+        this.q.forEach(req => req.callback(err))
+        this.q = []
+        this.emit('error', err)
+      } else {
+        this.ecc = ecc 
+        this.emit('ready')
+        if (this.q.length) this._run()
+      }
+    })
+  }
+
+  init (busNum, callback) {
+    let bus = i2c.open(busNum, err => {
+      if (err) {
+        callback(err)
+      } else {
+        let ecc = new ECC(bus)
+        ecc.scan(err => {
+          if (err) {
+            ecc.close()
+            callback(err)
+          } else {
+            ecc.busNum = busNum
+            ecc.preset({}, err => {
+              if (err) {
+                ecc.close()
+                callback(err)
+              } else {
+                callback(null, ecc)
+              }
+            })
+          }
+        })
+      }
+    }) 
+  }
+
+  _run () {
+    let { opts, callback } = this.q[0]
+    this.ecc[opts.op](opts, (err, data) => {
+      if (!this.q.length) return
+      this.q.shift()
+      if (this.q.length) this._run()
+      callback(err, data)
+    })
+  }
+
+  run (opts, callback) {
+    this.q.push({ opts, callback })
+    if (this.q.length === 1) this._run()
+  }
+
+  preset (callback = () => {}) {
+    this.run({ op: 'preset' }, callback)
+  }
+
+  sign (opts, callback) {
+    this.run(Object.assign({ op: 'sign' }, opts), (err, sig) => {
+      if (err) {
+        callback(err)
+      } else if (opts.der) {
+        callback(null, Signature(sig))
+      } else {
+        callback(null, sig)
+      }
+    })
+  }
+
+  verify (opts, callback) {
+    this.run(Object.assign({ op: 'verify' }, opts), callback)
+  }
+
+  genCsr (opts, callback) {
+    this.run(Object.assign({ op: 'genCsr' }, opts), callback)
+  }
+
+  serialNumber (opts, callback) {
+    this.run(Object.assign({ op: 'serialNumber' }, opts), callback)
+  }
+
+  readCounter (opts, callback) {
+    this.run(Object.assign({ op: 'readCounter' }, opts), callback)
+  }
+
+  incCounter (opts, callback) {
+    this.run(Object.assign({ op: 'incCounter' }, opts), callback)
+  }
+}
+
+/**
+const initEcc = (busNum, callback) => {
+  let bus = i2c.open(busNum, err => {
+    if (err) return callback(err)
+    let ecc = new ECC(bus)
+    ecc.scan(err => {
+      if (err) {
+        ecc.close()
+        callback(err)
+      } else {
+        ecc.busNum = busNum
+        callback(null, new Wrapper(ecc))
+      }
+    })
+  })
+}
+*/
+
+module.exports = new Wrapper(config.ecc.bus)
