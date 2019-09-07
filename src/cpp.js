450cf1677e166e0fb41c3b76377134d9cd757514
matianfu <matianfu@gmail.com>
2019-09-07 20:27:04 +0800

    update cpp

--- src/cpp.js
+++ src/cpp.js
@@ -2,27 +2,13 @@
 const path = require('path')
 process.chdir(path.dirname(__dirname))
 
-const config = require('config') 
-
-const bled = require('./components/bled')
-const ecc = require('../lib/atecc/atecc')
-const led = require('./components/led')
-const sata = require('./components/diskman')
-const nm = require('./components/nm')
-const http = require('./components/http')
-const localAuth = require('./components/localAuth')
-const owner = require('./components/owner')
-
-// prepare dirs and local constants
-const initDirs = callback => {
-  // set LED 
-}
-
-ecc.on('error', () => {
-  // set LED
+const device = require('./components/device')
+device.once('ready', () => {
+  require('./ble-app')
+  require('./http-app')
+  require('./components/responder')
 })
 
-sata.on('status', status => {
-  // set LED
+device.on('error', () => {
 })
-  
+
