2b667814fd3aabd9bff29d938f78987db4ed2ee9
matianfu <matianfu@gmail.com>
2019-09-18 15:45:22 +0800

    wip

--- src/actions/connect-wifi-and-bind.js
+++ src/actions/connect-wifi-and-bind.js
@@ -2,39 +2,62 @@ const os = require('os')
 const racer = require('../lib/racer')
 const device = require('../components/device')
 const ownership = require('../components/ownership')
+const watson = require('../components/watson')
+const channel = require('../components/channel')
 const connect = require('./connect-wifi')
+
+const logE = err => {
+  const { message, code, reason } = err
+  console.log('connect-wifi-and-bind:', { message, code, reason })
+}
+
+const logD = data => console.log('connect-wifi-and-bind:', data)
+const logI = (...args) => console.log('connect-wifi-and-bind:', ...args)
 
 // the last response called multiple times
 // and this is not a good design (no lazy, no end)
 module.exports = (ssid, password, encrypted, respond) =>
   connect(ssid, password, err => {
     if (err) {
-      err.reason = err.code
-      err.code = 'EWIFI'
       respond(err)
+      logE(err)
     } else {
       // progress
-      respond({ success: 'WIFI' })
+      const data = { success: 'WIFI' }
+      respond(data)
+      logD(data)
+
+      logI('channel.status', channel.status)
+      if (channel.status !== 'Connecting') channel.reconnect()
+
       const r = racer()
       setTimeout(r(() => {
         const err = new Error('cloud not connected in 60 seconds')
         err.code = 'ETIMEOUT'
         respond(err)
+        logE(err)
       }), 60 * 1000)
+
       ownership.on('owner', r(owner => {
         if (owner) {
           const err = new Error('owner exists')
           err.code = 'EEXIST'
           respond(err)
+          logE(err)
         } else {
-          respond({ success: 'CHANNEL' })
+          // progress
+          const data = { success: 'CHANNEL' }
+          respond(data)
+          logD(data)
+
           ownership.bind(encrypted, err => {
             if (err) {
               err.reason = err.code
               err.code = 'EBOUND'
               respond(err)
+              logE(err)
             } else {
-              respond({
+              const data = {
                 success: 'BOUND',
                 data: {
                   sn: device.sn,
@@ -44,7 +67,9 @@ module.exports = (ssid, password, encrypted, respond) =>
                     addr = ip ? ip.address : '0.0.0.0'
                   ) => addr)()
                 }
-              })
+              }
+              respond(data)
+              logD(data)
             }
           })
         }
--- src/actions/connect-wifi.js
+++ src/actions/connect-wifi.js
@@ -5,10 +5,23 @@ const readline = require('readline')
 const strip = require('strip-ansi')
 
 const iwfind = require('../lib/iwfind')
+const watson = require('../components/watson')
 
-// const device = require('../components/device')
+const logE = err => {
+  const { message, code, reason, bssid, status } = err
+  console.log('connect-wifi:', { message, code, reason, bssid, status })
+}
 
-// TODO error code
+const logD = data => console.log('connect-wifi:', data)
+
+/**
+each Error object has
+1. code, set to EWIFI
+2. reason,
+3. message
+4. bssid (optional)
+5. status (optional)
+*/
 
 /**
 ssid: string
@@ -19,54 +32,105 @@ const connect = (ssid, password, callback) => {
   console.log(`scan wifi and find ${ssid}`)
 
   iwfind('wlan0', ssid, 3, (err, found) => {
-    if (err || !found) {
-      if (err) {
-        console.log('iwfind error', err.message)
-      } else {
-        console.log(`${ssid} not found`)
-      }
+    if (err) {
 
-      const e = new Error('ssid not found')
-      e.code = 'EWIFI'
-      e.reason = 'ENOENT'
-      callback(e)
+      console.log('iwfind error', err.message)
 
+      err.code = 'EWIFI'
+      err.reason = 'EINTERNAL'
+      callback(err)
+      logE(err)
+    } else if (!found) {
+
+      console.log(`No network with SSID '${ssid}' found`)
+
+      const err = new Error(`No network with SSID '${ssid}' found`)
+      err.code = 'EWIFI'
+      err.reason = 'ENOTFOUND'
+      callback(err)
+      logE(err)
     } else {
-      console.log(`nmcli connect wifi ${ssid}`)
+
+      console.log(`nmcli connecting wifi ${ssid}`)
 
       const wpa = child.spawn('journalctl', ['-u', 'wpa_supplicant', '-f'])
       const rl = readline.createInterface({ input: wpa.stdout })
+      let assocRej = null
       rl.on('line', l => {
-        if (l.includes('wlan0:')) console.log(l.trim())
-      })
-
-      child.exec(`nmcli d wifi connect ${ssid} password ${password}`, (err, stdout, stderr) => {
-        if (err) {
-
-          console.log(`nmcli connect wifi failed`)
-          console.log(err.message)
-          console.log(strip(stderr.toString().trim()))
-
-          callback(err)
-        } else {
-          console.log(`nmcli connect wifi succeeded`)
-
-          // dns is available after nmcli finishes.
-          // restart timesyncd to force ntp update immediately
-          child.exec('systemctl restart systemd-timesyncd.service', () => {})
-          // sync file system
-          child.exec('sync', () => {})
-
-          let data = { address: '0.0.0.0' }
-          const wlan0 = os.networkInterfaces().wlan0
-          const ip = wlan0 && wlan0.find(x => x.family === 'IPv4')
-          if (ip) {
-            const { address, netmask, mac, cidr } = ip
-            data = { address, netmask, mac, prefix: cidr.split('/')[1] }
+        if (l.includes('wlan0: CTRL-EVENT-ASSOC-REJECT')) {
+          const xs = l.trim().split(' ').map(phr => phr.trim()).filter(phr => phr.startsWith('bssid=') || phr.startsWith('status_code'))
+          if (xs.length !== 2) return
+          assocRej = {
+            bssid: xs[0].slice('bssid='.length),
+            status: parseInt(xs[1].slice('status_code='.length))
           }
-          callback(null, data)
         }
       })
+
+      const nmcli = child.exec(`nmcli d wifi connect ${ssid} password ${password}`, 
+        (err, stdout, stderr) => {
+          if (err) {
+
+            console.log(`nmcli connect wifi failed`, err.mesage)
+
+            err.code = 'EWIFI'
+            err.reason = 'EINTERNAL'
+            callback(err)
+            logE(err)
+          } else {
+
+            /** 
+            nmcli exits with exitCode 0 and signalCode null even in error
+            Device 'wlp2s0' successfully activated with '68d5ccc1-f9f0-45c4-82b2-a22fe9ddb518'.
+            */
+
+            /*
+            console.log(`nmcli connect wifi succeeded`)
+            console.log('------ stdout ------')
+            console.log(strip(stdout.toString()))
+            console.log('------ stderr ------')
+            console.log(strip(stderr.toString()))    
+            console.log('------ nmcli ------')
+            console.log(nmcli)
+            */
+
+            const first = strip(stdout.toString()).split('\n')[0]
+
+            if (!first.includes('successfully activated')) {
+
+              const err = new Error(first)
+              err.code = 'EWIFI'
+              if (assocRej) {
+                err.reason = 'EASSOCREJ'
+                Object.assign(err, assocRej)
+              } else {
+                err.reason = 'EFAIL'
+              }
+
+              callback(err)
+              logE(err)
+            } else {
+
+              // dns is available after nmcli finishes.
+              // restart timesyncd to force ntp update immediately
+              // child.exec('systemctl restart systemd-timesyncd.service', () => {})
+              // sync file system
+              child.exec('sync', () => {})
+              setTimeout(() => child.exec('sync', () => {}), 3 * 1000)
+
+              let data = { address: '0.0.0.0' }
+              const wlan0 = os.networkInterfaces().wlan0
+              const ip = wlan0 && wlan0.find(x => x.family === 'IPv4')
+              if (ip) {
+                const { address, netmask, mac, cidr } = ip
+                data = { address, netmask, mac, prefix: cidr.split('/')[1] }
+              }
+
+              callback(null, data)
+              logD(data)
+            }
+          }
+        })
     }
   })
 }
